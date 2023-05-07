eb64f56e6f677ab8aad99a0716142770d0bcbc35
SillyCodeDev <bartoc.junior@gmail.com>
2023-05-07 15:43:25 +0200

    c++ finish

--- C++/Calc.cpp
+++ C++/Calc.cpp
@@ -3,15 +3,23 @@
 using namespace std;
 
 int calc(char op, string in){
-    switch (op)
-    {
-    case '+':
+    string i1 = in.substr(0, in.find(op));
+    string i2 = in.substr(in.find(op) + 1, in.length());
+    switch (op) {
+        case '+':
+            return stoi(i1) + stoi(i2);
 
-        break;
-    
-    default:
+        case '-':
+           return stoi(i1) - stoi(i2);
 
-        break;
+        case '/':
+            return stoi(i1) / stoi(i2);
+        
+        case '*':
+            return stoi(i1) * stoi(i2);
+
+        case '%':
+            return stoi(i1) % stoi(i2);
     }
     return 0;
 }
@@ -26,27 +34,29 @@ int main(){
         cout << ">";
         cin >> in;
 
-        if(in.find("+") != 18446744073709551615){
-            calc('+', in);
+        if(in.find("+") != 18446744073709551615 && in.find("+") != 0){
+            cout << calc('+', in);
 
-        } else if(in.find("-") != 18446744073709551615){
-            calc('-', in);
+        } else if(in.find("-") != 18446744073709551615 && in.find("+") != 0){
+            cout << calc('-', in);
 
-        } else if(in.find("/") != 18446744073709551615){
-              calc('/', in);
+        } else if(in.find("/") != 18446744073709551615 && in.find("+") != 0){
+            cout << calc('/', in);
   
-        } else if(in.find("*") != 18446744073709551615){
-             calc('/', in);
+        } else if(in.find("*") != 18446744073709551615 && in.find("+") != 0){
+            cout << calc('*', in);
   
-        } else if(in.find("%") != 18446744073709551615){
-            calc('%', in);
+        } else if(in.find("%") != 18446744073709551615 && in.find("+") != 0){
+            cout << calc('%', in);
 
-        } else if(in.find("exit") != 18446744073709551615){
+        } else if(in.find("exit") != 18446744073709551615 && in.find("+") != 0){
             return 0;
 
         } else {
-
+            cout << "No correct placed operator or not supported operator.";
         }
+
+        cout << "\n";
     }
     
     return 0;
